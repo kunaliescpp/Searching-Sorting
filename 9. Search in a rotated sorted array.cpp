40c81a9a3f8ec978c997e934073390e336b1815a
kunal_cpp <kunaliesc@gmail.com>
2022-02-26 11:27:34 +0530

    Update 9. Search in a rotated sorted array.cpp

--- 9. Search in a rotated sorted array.cpp
+++ 9. Search in a rotated sorted array.cpp
@@ -11,8 +11,6 @@ that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 
 if it is not in nums.
 
-You must write an algorithm with O(log n) runtime complexity.
-
 Example 1:
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
