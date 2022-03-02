36bc7386c45c66422cb4dc052fd049a40bda3819
kunal_cpp <kunaliesc@gmail.com>
2022-03-02 15:41:28 +0530

    Update 8.1 Merge sort.cpp

--- 8.1 Merge sort.cpp
+++ 8.1 Merge sort.cpp
@@ -13,47 +13,51 @@ Example 2:
 Input: N = 10, arr[] = {10 9 8 7 6 5 4 3 2 1}
 Output: 1 2 3 4 5 6 7 8 9 10
 
+
 Constraints:
 1 <= N <= 10^5
 1 <= arr[i] <= 10^3
 */
 
+//author: kunaliescpp
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long int
+#define mod 1000000007
 
-void merge(vector<int>& v, int si, int ei){
-    if(si >= ei) return;
+void solver(vector<int>& v, int l, int r){
     
-    int mid = (si + ei)/2;
-    merge(v, si, mid);
-    merge(v, mid+1, ei);
+    if(l >= r) return;
 
-    int i = si, j = mid+1;
-    vector<int> ans;
-    while(i <= mid && j <= ei){
-        if(v[i] > v[j]) ans.push_back(v[j++]);
-        else ans.push_back(v[i++]);
+    int mid = (l+r)/2;
+    solver(v, l, mid);
+    solver(v, mid+1, r);
+    
+    vector<int> store;
+    int i = l, j = mid+1;
+    while(i <= mid && j <= r){
+        if(v[i] <= v[j]) store.push_back(v[i++]);
+        else store.push_back(v[j++]);
     }
     
-    while(i <= mid) ans.push_back(v[i++]);
-    while(j <= end) ans.push_back(v[j++]);
+    while(i <= mid) store.push_back(v[i++]);
+    while(j <= r) store.push_back(v[j++]);
     
-    for(int i = si; i <= ei; i++){
-        v[i] = ans[i-si];
+    for(int ci = 0; ci < store.size(); ci++){
+        v[ci+l] = store[ci];
     }
 }
 
-int main() {
-	
+int32_t main() {
+
     int n; cin >> n;
-
-    vector<int> v(n);
-    for(int &i : v) cin >> i;
-
-    merge(v, 0, n-1);
-    for(int &i: v) cout << v[i] << " ";
-	
-return 0;
+      
+    vector<int>v(n);
+    for(int &i: v) cin >> i;
+    solver(v, 0, n-1);
+    for(int &i: v) cout << i << " ";
+  
+  return 0;
 }
 
 
